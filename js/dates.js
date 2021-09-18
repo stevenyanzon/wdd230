2ed901b939ef50f9fa1124f6f07ecd2e4eef739c
stevenyanzon <yan21006@byui.edu>
2021-09-19 02:12:26 +0800

    Updated work

---
+++ js/dates.js
@@ -0,0 +1,5 @@
+const copyrightyear = document.getElementById("copyrightyear");
+copyrightyear.textContent = new Date().getFullYear();
+
+const timeupdate = document.getElementById("timeupdate");
+timeupdate.textContent = (document.lastModified);
