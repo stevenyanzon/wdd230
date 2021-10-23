4d576d45876e8310678d549f4836c2cef5f19135
stevenyanzon <yan21006@byui.edu>
2021-10-23 23:51:35 +0800

    changes to js

--- lesson6/js/windchill.js
+++ lesson6/js/windchill.js
@@ -3,16 +3,16 @@ const speed = parseFloat(document.querySelector("#speed").textContent);
 
 const wc = 35.74 + 0.6215 * temp - 35.75 * Math.pow(speed, 0.16) + 0.4275 * temp * Math.pow(speed, 0.16);
 
-let windchill = Math.floor(wc);
+let windChill = Math.floor(wc);
 
 
 if(speed > 3 && temp <= 50){
-    let istemp = isNaN(temp);
-    let isspeed = isNaN(speed);
-    if(istemp === "True" || isspeed === "True"){
+    let wTemp = isNaN(temp);
+    let wSpeed = isNaN(speed);
+    if(wTemp === "True" || wSpeed === "True"){
         document.querySelector("#chill").innerHTML = `N/A`;
     }else{
-        document.querySelector("#chill").innerHTML = `${windchill}`; 
+        document.querySelector("#chill").innerHTML = `${windChill}`; 
     }
 }else {
     document.querySelector("#chill").innerHTML = `N/A`;
