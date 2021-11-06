5061bbec0e98a880ef24477fed4a28e2d77944bf
stevenyanzon <yan21006@byui.edu>
2021-11-06 17:18:09 +0800

    added storm center

---
+++ lesson8/js/scripts.js
@@ -0,0 +1,7 @@
+function toggleMenu() {
+  document.getElementById("primaryNav").classList.toggle('hidenav');
+}
+
+function adjustRating(rating) {
+  document.getElementById("ratingvalue").innerHTML = rating;
+}
