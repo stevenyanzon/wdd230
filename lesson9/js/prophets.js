a66ad81f5fd3d78c3757855050e159a8d10dcb5f
stevenyanzon <yan21006@byui.edu>
2021-11-09 23:44:47 +0800

    updated week7 assignment

---
+++ lesson9/js/prophets.js
@@ -0,0 +1,13 @@
+const requestURL = 'https://byui-cit230.github.io/lessons/lesson-09/data/latter-day-prophets.json';
+
+fetch(requestURL)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (jsonObject) {
+    console.table(jsonObject);  // temporary checking for valid response and data parsing
+    for (let i = 0; i < prophets.length; i++ ) {
+      
+  });
+
+const prophets = jsonObject['prophets'];
