a2665196e1d162f3c124d4d43db9ab3c2b866f11
stevenyanzon <yan21006@byui.edu>
2021-11-11 23:40:36 +0800

    added week 9 prophet card

--- lesson9/js/prophets.js
+++ lesson9/js/prophets.js
@@ -5,9 +5,42 @@ fetch(requestURL)
     return response.json();
   })
   .then(function (jsonObject) {
-    console.table(jsonObject);  // temporary checking for valid response and data parsing
+    const prophets = jsonObject['prophets'];
+    
+    //loop through every record
     for (let i = 0; i < prophets.length; i++ ) {
+
+      //create HTML Element
+      let card = document.createElement("section");
+      let h2 = document.createElement("h2");
+      let bday = document.createElement("p");
+      let bplace = document.createElement("p");
+      let image = document.createElement("img");
+
+      //set order of prophets 
+      card.setAttribute("class", `order${prophets[i].order}`)
+
+      //pass the value of h2
+      h2.textContent = `${prophets[i].name} ${prophets[i].lastname}`;
+
+      //Value of Birthdate
+      bday.textContent = `Birth Date: ${prophets[i].birthdate}`;
+
+      //Value of Birthplace
+      bplace.textContent = `Birth Place: ${prophets[i].birthplace}`;
+
+      //set src and alt to img
+      image.setAttribute("src", prophets[i].imageurl);
+      image.setAttribute("alt", `${prophets[i].name} ${prophets[i].lastname} - ${prophets[i].order}`);
+
+      //append the values
+      card.appendChild(h2);
+      card.appendChild(bday);
+      card.appendChild(bplace);
+      card.appendChild(image);
+
+      document.querySelector("div.cards").appendChild(card);
       
+    }
   });
 
-const prophets = jsonObject['prophets'];
