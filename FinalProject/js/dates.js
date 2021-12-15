fba46c25c9552bef37382f471734d60341105017
stevenyanzon <yan21006@byui.edu>
2021-12-16 06:00:10 +0800

    time update

--- FinalProject/js/dates.js
+++ FinalProject/js/dates.js
@@ -27,3 +27,9 @@ function bannerBox() {
     }
     }
     bannerBox();
+
+const copyrightyear = document.getElementById("copyrightyear");
+copyrightyear.textContent = new Date().getFullYear();
+    
+const timeupdate = document.getElementById("timeupdate");
+timeupdate.textContent = (document.lastModified); 
