c33637c1d44fd81a3f1e4df6bedfe7004409ad64
stevenyanzon <yan21006@byui.edu>
2021-12-16 06:03:34 +0800

    final output

--- FinalProject/js/dates.js
+++ FinalProject/js/dates.js
@@ -28,8 +28,4 @@ function bannerBox() {
     }
     bannerBox();
 
-const copyrightyear = document.getElementById("copyrightyear");
-copyrightyear.textContent = new Date().getFullYear();
-    
-const timeupdate = document.getElementById("timeupdate");
-timeupdate.textContent = (document.lastModified); 
+
